459ad634de9417ea46aeab5b7a35036ac2843f05
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2024-04-25 17:19:42 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+USER=''
+APP_PASSWORD=''
+IMAP_URL='imap.gmail.com'
+SMTP_URL='smtp.gmail.com'
+SMTP_PORT=587
+
+SYSTEM_PROMPT='''
+<your prompt>
+You ALWAYS respect the client.
+Your response format must only be what you intend to reply with to the email.
+'''
+MODEL='llama3-70b-8192'
+GROQ_API_KEY=''
+EMB_MODEL='mxbai-embed-large'
