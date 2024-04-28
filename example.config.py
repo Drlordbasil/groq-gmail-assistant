1e2e404b4dda8c7bd49d67d5922423a84d492cd2
Lord Basil - Automate EVERYTHING <126736516+Drlordbasil@users.noreply.github.com>
2024-04-28 13:41:24 -0400

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -1,9 +1,14 @@
+###############################################
+# User = Email address ie drlordbasil@gmail.com
+#########
+
 USER=''
+#app password from google apps.
 APP_PASSWORD=''
 IMAP_URL='imap.gmail.com'
 SMTP_URL='smtp.gmail.com'
 SMTP_PORT=587
-
+### This prompt can vary but generally try to keep him on track. 
 SYSTEM_PROMPT='''
 <your prompt>
 You ALWAYS respect the client.
